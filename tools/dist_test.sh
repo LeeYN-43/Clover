9afa3c0a1bfc007e485bb8ab44fc87d6aea476ae
LeeYN-43 <liyinan1998@126.com>
2022-11-15 18:43:13 +0800

    tools/ddist_train.sh
    update

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -8,5 +8,7 @@ export BYTED_TORCH_FX=O0
 
 # PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 # Arguments starting from the forth one are captured by ${@:4}
+# export CUDA_VISIBLE_DEVICES=0,1
+
 torchrun --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
